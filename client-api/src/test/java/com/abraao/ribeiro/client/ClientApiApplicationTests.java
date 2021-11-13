56961bf8b7090a3022421ec7cca35a0e65c76ffd
abraaoribeiro <abraaohdg@gmail.com>
2021-11-13 08:57:15 -0300

    first commit

---
+++ client-api/src/test/java/com/abraao/ribeiro/client/ClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraao.ribeiro.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-api/src/test/java/com/abraao/ribeiro/payment/PaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraao.ribeiro.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
