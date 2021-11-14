a55710143f3df9977f41ba7024ecbd213e646328
abraaoribeiro <abraaohdg@gmail.com>
2021-11-14 09:50:26 -0300

    feat: Configura o server discovery
    Foi adicionado o server discovery Eureka

---
+++ eureka-server/src/test/java/com/abraao/ribeiro/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abraao.ribeiro.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
