3350e88b825a86332b511db4f61ac1f54ff0f159
abraaoribeiro <abraaohdg@gmail.com>
2021-11-19 00:13:08 -0300

    refactor: Update Script start.sh

--- account-api/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO account.bank (name, number,created_at,update_at) values ("Itáu", "144", '2021-05-08 21:31:32', '2021-05-08 21:31:32');
-INSERT INTO account.bank (name, number,created_at, update_at) values ("Banco do Brasil", "522", '2021-05-08 21:31:32', '2021-05-08 21:31:32');
-
